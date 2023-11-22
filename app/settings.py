86afc37fa3ba5aa53523611e357212dd7069d2b6
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2023-11-22 14:16:03 -0300

    refact: changing the project's architecture

--- app/settings.py
+++ app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c!!(=e6o+bz^f0%j5+t_3*@_j+88i=wdlinoi9+6wjgro)7j*+'
+SECRET_KEY = 'django-insecure-8e*2ky0yus_(_gvmlj2cnlsb07ezcd(-&2lexiv&ga*13rs0x+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
+        'HOST': os.environ.get('DB_HOST'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
     }
