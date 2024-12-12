3fef2ef8fe9b6f2d5bcecd65dfd4c23a5c9cddb7
Lucas Araújo de Medeiros <100412915+lucasaraujo1301@users.noreply.github.com>
2024-12-12 16:52:51 -0300

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("create/", views.CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token"),
     path("me/", views.ManageUserView.as_view(), name="me"),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
